e24cb6e84df1ece37a8fed05242fa0cfba33d968
syarotta1997 <helenlu1997@gmail.com>
2018-06-27 16:45:00 -0400

    initialize

---
+++ my-app/src/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './home.css'
+
+class Home extends Component {
+    render(){
+        return(
+            <div className="container">
+                <h1>Portfolio</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './home.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+    <Home />,
+    document.getElementById('root')
+  );
